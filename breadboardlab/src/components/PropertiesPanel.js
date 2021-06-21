628c19d3b8272cbb4e03f01e5f3abcd1d0090fad
SethCohen <sethcohen.dev@gmail.com>
2021-06-21 15:05:13 -0400

    #3 Changes Properties Panel visibility mechanics.
    - Switched from css styling visibility to if components should even be added to the DOM.

--- breadboardlab/src/components/PropertiesPanel.js
+++ breadboardlab/src/components/PropertiesPanel.js
@@ -6,16 +6,6 @@ const useStyles = makeStyles((theme) => ({
     grid: {
         margin: theme.spacing(1),
     },
-    colourHide: {
-        display: 'none',
-    },
-    resistanceHide: {
-        display: 'none',
-    },
-    capacitanceHide: {
-        display: 'none',
-    },
-
 }));
 
 const PropertiesPanel = props => {
@@ -41,45 +31,51 @@ const PropertiesPanel = props => {
                             onChange={props.handleTextField}
                         />
                     </Grid>
-                    <Grid item className={!props.partData.colourEnabled && classes.colourHide}>
-                        <InputLabel id="part-colour">Colour</InputLabel>
-                        <Select
-                            labelId="part-colour"
-                            id="part-colour"
-                            value={props.partData.colour}
-                            onChange={props.handleColourChange}
-                        >
-                            <MenuItem value={"red"}>Red</MenuItem>
-                            <MenuItem value={"green"}>Green</MenuItem>
-                            <MenuItem value={"blue"}>Blue</MenuItem>
-                        </Select>
-                    </Grid>
-                    <Grid item className={!props.partData.resistanceEnabled && classes.resistanceHide}>
-                        <InputLabel id="part-resistance">Resistance</InputLabel>
-                        <Select
-                            labelId="part-resistance"
-                            id="part-resistance"
-                            value={props.partData.resistance}
-                            onChange={props.handleResistanceChange}
-                        >
-                            <MenuItem value={10}>10 Ω</MenuItem>
-                            <MenuItem value={330}>330 Ω</MenuItem>
-                            <MenuItem value={1000}>1 kΩ</MenuItem>
-                        </Select>
-                    </Grid>
-                    <Grid item className={!props.partData.capacitanceEnabled && classes.capacitanceHide}>
-                        <InputLabel id="part-capacitance">Capacitance</InputLabel>
-                        <Select
-                            labelId="part-capacitance"
-                            id="part-capacitance"
-                            value={props.partData.capacitance}
-                            onChange={props.handleCapacitanceChange}
-                        >
-                            <MenuItem value={100}>100 nF</MenuItem>
-                            <MenuItem value={200}>200 nF</MenuItem>
-                            <MenuItem value={300}>300 nF</MenuItem>
-                        </Select>
-                    </Grid>
+                    {props.partData.colourEnabled ?
+                        <Grid item>
+                            <InputLabel id="part-colour">Colour</InputLabel>
+                            <Select
+                                labelId="part-colour"
+                                id="part-colour"
+                                value={props.partData.colour}
+                                onChange={props.handleColourChange}
+                            >
+                                <MenuItem value={"red"}>Red</MenuItem>
+                                <MenuItem value={"green"}>Green</MenuItem>
+                                <MenuItem value={"blue"}>Blue</MenuItem>
+                            </Select>
+                        </Grid>
+                        : null}
+                    {props.partData.resistanceEnabled ?
+                        <Grid item>
+                            <InputLabel id="part-resistance">Resistance</InputLabel>
+                            <Select
+                                labelId="part-resistance"
+                                id="part-resistance"
+                                value={props.partData.resistance}
+                                onChange={props.handleResistanceChange}
+                            >
+                                <MenuItem value={10}>10 Ω</MenuItem>
+                                <MenuItem value={330}>330 Ω</MenuItem>
+                                <MenuItem value={1000}>1 kΩ</MenuItem>
+                            </Select>
+                        </Grid>
+                        : null}
+                    {props.partData.capacitanceEnabled ?
+                        <Grid item>
+                            <InputLabel id="part-capacitance">Capacitance</InputLabel>
+                            <Select
+                                labelId="part-capacitance"
+                                id="part-capacitance"
+                                value={props.partData.capacitance}
+                                onChange={props.handleCapacitanceChange}
+                            >
+                                <MenuItem value={100}>100 nF</MenuItem>
+                                <MenuItem value={200}>200 nF</MenuItem>
+                                <MenuItem value={300}>300 nF</MenuItem>
+                            </Select>
+                        </Grid>
+                        : null}
                 </Grid>
             </Grid>
         </Container>
