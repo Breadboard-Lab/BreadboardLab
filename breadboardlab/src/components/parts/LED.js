75a20ba4edd959fede744cacf79a03135337dd0b
SethCohen <sethcohen.dev@gmail.com>
2021-06-30 12:50:17 -0400

    #3, Added visual colour change to LED on properties change.

--- breadboardlab/src/components/parts/LED.js
+++ breadboardlab/src/components/parts/LED.js
@@ -5,26 +5,27 @@ export default class LED extends React.Component {
     constructor(props) {
         super(props);
         this.node = React.createRef();
-        
+
         this.state = {
             type: "LED",
             name: "LED",
             colour: "Red",
-            colourEnabled: true,
+            visualColourOuter: "hsl(0, 100%, 31%)",
+            visualColourInner: "hsl(0, 100%, 75%)"
         }
         this.onDoubleClick = this.onDoubleClick.bind(this);
         this.updateProp = this.updateProp.bind(this);
-        
+
         this.scale = {x: 20, y: 20};
         this.offSet = {x: 0.3, y: 0.5};
     }
 
     componentDidMount() {
         interact(this.node.current.parentNode).styleCursor(false).draggable({
-			listeners: {
-				move: this.props.movePart
-			},
-		})
+            listeners: {
+                move: this.props.movePart
+            },
+        })
     }
 
     onDoubleClick() {
@@ -38,32 +39,60 @@ export default class LED extends React.Component {
             this.setState({name: value}, this.onDoubleClick);
         } else if (propName.toLowerCase() === "colour") {
             this.setState({colour: value}, this.onDoubleClick);
+
+            // Changes LED colour based on Properties Panel Colour Selection
+            switch (value) {
+                case "Red":
+                    this.setState({visualColourOuter: "hsl(0, 100%, 31%)"})
+                    this.setState({visualColourInner: "hsl(0, 100%, 75%)"})
+                    break
+                case "Blue":
+                    this.setState({visualColourOuter: "hsl(210,100%,31%)"})
+                    this.setState({visualColourInner: "hsl(210,100%,75%)"})
+                    break
+                case "Green":
+                    this.setState({visualColourOuter: "hsl(100, 100%, 31%)"})
+                    this.setState({visualColourInner: "hsl(125,100%,75%)"})
+                    break
+                case "Yellow":
+                    this.setState({visualColourOuter: "hsl(55,100%,31%)"})
+                    this.setState({visualColourInner: "hsl(55, 100%, 75%)"})
+                    break
+                default:
+                    this.setState({visualColourOuter: "hsl(0, 100%, 31%)"})
+                    this.setState({visualColourInner: "hsl(0, 100%, 75%)"})
+            }
         }
     }
 
     getProps() {
-        return(
+        return (
             {
                 ref: this,
                 callback: this.updateProp,
-                props:  [
+                props: [
                     {propName: "Type", propType: "string", value: this.state.type},
                     {propName: "Name", propType: "textfield", value: this.state.name},
-                    {propName: "Colour", propType: "select", value: this.state.colour, options: ["Red", "Blue", "Green", "Yellow"]}
+                    {
+                        propName: "Colour",
+                        propType: "select",
+                        value: this.state.colour,
+                        options: ["Red", "Blue", "Green", "Yellow"]
+                    }
                 ]
             }
 
         )
     }
-    
+
     render() {
-        return(
+        return (
             <g ref={this.node} onDoubleClick={this.onDoubleClick} transform="translate(17,28) scale(40,40)">
                 <path d="M 0 0.8 A 0.7 0.7 90 1 1 0.8 0.8 Z"
-                        fill="#a00000" strokeOpacity="0" />
-                <circle cx="0.4" cy="0.2" r="0.6" fill="#ff8080" />
-                <circle cx="0.4" cy="-0.1" r="0.08" fill="#707070" />
-                <circle cx="0.4" cy="0.5" r="0.08" fill="#707070" />
+                      fill={this.state.visualColourOuter} strokeOpacity="0"/>
+                <circle cx="0.4" cy="0.2" r="0.6" fill={this.state.visualColourInner}/>
+                <circle cx="0.4" cy="-0.1" r="0.08" fill="#707070"/>
+                <circle cx="0.4" cy="0.5" r="0.08" fill="#707070"/>
             </g>
         )
     }
