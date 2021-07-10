86854f88be0b41c315d5ce0e5c3350b2023c99a1
Benedict Leung <benedict.leung1@ontariotechu.net>
2021-07-10 19:58:32 -0400

    Changed LED SVG and implemented moving cathode and anode

--- breadboardlab/src/components/parts/LED.js
+++ breadboardlab/src/components/parts/LED.js
@@ -6,6 +6,8 @@ export default class LED extends React.Component {
     constructor(props) {
         super(props);
         this.node = React.createRef();
+        this.cathode = React.createRef();
+        this.anode = React.createRef();
 
         this.state = {
             type: "LED",
@@ -13,6 +15,8 @@ export default class LED extends React.Component {
             colour: "Red",
             visualColourOuter: "hsl(0, 100%, 31%)",
             visualColourInner: "hsl(0, 100%, 75%)",
+            cathodePoint: {x: 0.1, y: 0.2},
+            anodePoint: {x: 0.7, y: 0.2},
             isSelected: false,
         }
         this.onDoubleClick = this.onDoubleClick.bind(this);
@@ -20,6 +24,25 @@ export default class LED extends React.Component {
 
         this.scale = {x: 20, y: 20};
         this.offSet = {x: 0.3, y: 0.5};
+    }
+
+    draggableOptionsCathode = {
+        listeners: {
+            move: (event) => {
+                let scale = document.getElementById("AppSVG").getAttribute("scale");
+                this.setState({cathodePoint: {x: Number(event.currentTarget.getAttribute("cx")) + event.delta.x * scale / this.scale.x, y: Number(event.currentTarget.getAttribute("cy")) + event.delta.y * scale / this.scale.y}});
+            }
+        }
+    }
+
+    draggableOptionsAnode = {
+        listeners: {
+            move: (event) => {
+                let scale = document.getElementById("AppSVG").getAttribute("scale");
+
+                this.setState({anodePoint: {x: Number(event.currentTarget.getAttribute("cx")) + event.delta.x * scale / this.scale.x, y: Number(event.currentTarget.getAttribute("cy")) + event.delta.y * scale / this.scale.y}});
+            }
+        }
     }
 
     componentDidMount() {
@@ -98,26 +121,32 @@ export default class LED extends React.Component {
     render() {
         return (
             <g ref={this.node} onDoubleClick={this.onDoubleClick} transform="translate(17,28) scale(40,40)">
+                <path stroke="#707070" strokeWidth="0.16" strokeLinecap="round" d={`M 0.1 0.2 L ${this.state.cathodePoint.x} ${this.state.cathodePoint.y}`}/>
+                <path stroke="#707070" strokeWidth="0.16" strokeLinecap="round" d={`M 0.7 0.2 L ${this.state.anodePoint.x} ${this.state.anodePoint.y}`}/>
+
                 <path d="M 0 0.8 A 0.7 0.7 90 1 1 0.8 0.8 Z"
-                      fill={this.state.visualColourOuter} strokeOpacity="0"/>
-                <circle cx="0.4" cy="0.2" r="0.6" fill={this.state.visualColourInner}/>
-                {/* <circle cx="0.4" cy="-0.1" r="0.08" fill="#707070"/>
-                <circle cx="0.4" cy="0.5" r="0.08" fill="#707070"/> */}
-                <path stroke="#707070" strokeWidth="0.16" strokeLinecap="round" d="M 0.4 -0.1 L 0.4 -0.1"/>
-                <path stroke="#707070" strokeWidth="0.16" strokeLinecap="round" d="M 0.4 0.5 L 0.4 0.5"/>
-                <Interactable styleCursor={false} draggable={true}>
+                      fill={this.state.visualColourInner} strokeWidth="0.1" strokeOpacity="1" fillOpacity="0.8"/>
+                <path d="M 0 0.8 A 0.7 0.7 90 1 1 0.8 0.8 Z"
+                      stroke={this.state.visualColourOuter} strokeWidth="0.1" strokeOpacity="1" fillOpacity="0"/>
+
+
+                <circle cx="0.1" cy="0.2" r="0.08" fill="#707070"/>
+                <circle cx="0.7" cy="0.2" r="0.08" fill="#707070"/>
+                <Interactable ref={this.cathode} styleCursor={false} draggable={true} draggableOptions={this.draggableOptionsCathode}>
                     <ellipse 
                         onMouseEnter={this.onMouseEnter}
                         onMouseLeave={this.onMouseLeave}
-                        strokeOpacity="0" fillOpacity="0" cx="0.4" cy="-0.1" rx="0.08" ry="0.08"/>
+                        className="connector" strokeOpacity="1" fillOpacity="0" cx={this.state.cathodePoint.x} cy={this.state.cathodePoint.y} rx="0.08" ry="0.08"/>
                 </Interactable>
-                <Interactable styleCursor={false} draggable={true}>
+
+                <Interactable ref={this.anode} styleCursor={false} draggable={true} draggableOptions={this.draggableOptionsAnode}>
                     <ellipse 
                         onMouseEnter={this.onMouseEnter}
                         onMouseLeave={this.onMouseLeave}
-                        strokeOpacity="0" fillOpacity="0" cx="0.4" cy="0.5" rx="0.08" ry="0.08"/>
+                        className="connector" strokeOpacity="1" fillOpacity="0" cx={this.state.anodePoint.x} cy={this.state.anodePoint.y} rx="0.08" ry="0.08"/>
                 </Interactable>
-                <path d="M 0 0.8 A 0.7 0.7 90 1 1 0.8 0.8 Z"
+
+                <path d="M -0.06 0.85 A 0.77 0.77 90 1 1 0.85 0.85 Z"
                       fill="none" stroke={this.state.isSelected ? "#2453ff" : "none"} strokeWidth="0.075" strokeMiterlimit="50" strokeLinecap="round" strokeLinejoin="round" />
             </g>
         )
