13f3b1dcddccbae467b441a06bfe72eb7c5a39ee
SethCohen <sethcohen.dev@gmail.com>
2021-07-23 16:01:21 -0400

    Added multiple 74xx series chips.
    - Added Quad AND chip
    - Added Quad NAND chip
    - Added Quad NOR chip
    - Added Quad OR chip
    - Added Quad XOR chip

---
+++ breadboardlab/src/components/parts/QuandXORChip.js
@@ -0,0 +1,96 @@
+import React from "react";
+import interact from "interactjs";
+
+export default class QuadXORChip extends React.Component {
+    constructor(props) {
+        super(props);
+        this.node = React.createRef();
+
+        this.state = {
+            type: "74HC86",
+            name: "Quad XOR Chip",
+            isSelected: false,
+        }
+        this.scale = {x: 1.75, y: 1.75};
+        this.offSet = {x: 0, y: 0};
+        this.onDoubleClick = this.onDoubleClick.bind(this);
+        this.updateProp = this.updateProp.bind(this);
+    }
+
+    componentDidMount() {
+        interact(this.node.current.parentNode).styleCursor(false).draggable({
+            listeners: {
+                move: this.props.movePart
+            },
+        })
+    }
+
+    onDoubleClick() {
+        this.props.onDoubleTap(this.getProps());
+    }
+
+    updateProp(propName, value) {
+        if (propName.toLowerCase() === "type") {
+            this.setState({type: value}, this.onDoubleClick);
+        } else if (propName.toLowerCase() === "name") {
+            this.setState({name: value}, this.onDoubleClick);
+        }
+    }
+
+    getProps() {
+        return (
+            {
+                ref: this,
+                callback: this.updateProp,
+                props: [
+                    {propName: "Type", propType: "string", value: this.state.type},
+                    {propName: "Name", propType: "textfield", value: this.state.name},
+                ]
+            }
+
+        )
+    }
+
+    render() {
+        return (
+            <g ref={this.node} onDoubleClick={this.onDoubleClick}>
+                <g id="pins">
+                    <path id="pin_x5F_14" fill="#606262" d="M3,23l1-4.582c0-0.552,0.448-1,1-1s1,0.448,1,1L7,23H3z"/>
+                    <path id="pin_x5F_13" fill="#606262" d="M12,23l1-4.582c0-0.552,0.448-1,1-1s1,0.448,1,1L16,23H12z"/>
+                    <path id="pin_x5F_12" fill="#606262" d="M21,23l1-4.582c0-0.552,0.448-1,1-1s1,0.448,1,1L25,23H21z"/>
+                    <path id="pin_x5F_11" fill="#606262" d="M30,23l1-4.582c0-0.552,0.448-1,1-1s1,0.448,1,1L34,23H30z"/>
+                    <path id="pin_x5F_10" fill="#606262" d="M39,23l1-4.582c0-0.552,0.447-1,1-1s1,0.448,1,1L43,23H39z"/>
+                    <path id="pin_x5F_9" fill="#606262" d="M48,23l1-4.582c0-0.552,0.447-1,1-1s1,0.448,1,1L52,23H48z"/>
+                    <path id="pin_x5F_8" fill="#606262" d="M57,23l1-4.582c0-0.552,0.447-1,1-1s1,0.448,1,1L61,23H57z"/>
+                    <path id="pin_x5F_7" fill="#606262" d="M61,41l-1,4.582c0,0.553-0.447,1-1,1s-1-0.447-1-1L57,41H61z"/>
+                    <path id="pin_x5F_6" fill="#606262" d="M52,41l-1,4.582c0,0.553-0.447,1-1,1s-1-0.447-1-1L48,41H52z"/>
+                    <path id="pin_x5F_5" fill="#606262" d="M43,41l-1,4.582c0,0.553-0.447,1-1,1s-1-0.447-1-1L39,41H43z"/>
+                    <path id="pin_x5F_4" fill="#606262" d="M34,41l-1,4.582c0,0.553-0.448,1-1,1s-1-0.448-1-1L30,41H34z"/>
+                    <path id="pin_x5F_3" fill="#606262" d="M25,41l-1,4.582c0,0.552-0.447,1-1,1s-1-0.448-1-1L21,41H25z"/>
+                    <path id="pin_x5F_2" fill="#606262" d="M16,41l-1,4.582c0,0.552-0.447,1-1,1s-1-0.448-1-1L12,41H16z"/>
+                    <path id="pin_x5F_1" fill="#606262" d="M7,41l-1,4.582c0,0.552-0.447,1-1,1s-1-0.448-1-1L3,41H7z"/>
+                </g>
+                <g id="body">
+                    <path fill="#303030" d="M63.5,40.999c0,0.553-0.449,1.001-1.002,1.001H1.5c-0.554,0-1-0.448-1-1.001V23c0-0.552,0.446-1,1-1h60.998
+		c0.553,0,1.002,0.448,1.002,1V40.999z"/>
+                    <path fill="#262626" d="M0.5,28.5C2.433,28.5,4,30.067,4,32s-1.567,3.5-3.5,3.5"/>
+                    <circle fill="#262626" cx="5" cy="38" r="1.75"/>
+                </g>
+                <text transform="matrix(1 0 0 1 15.667 34.9219)" fill="#EFEFEF" fontFamily="Roboto" fontSize="10"
+                      fontStretch="condensed" fontWeight="bold">74HC86
+                </text>
+                <path id="select" fill="none" stroke={this.state.isSelected ? "#2453ff" : "none"} strokeLinecap="round"
+                      strokeLinejoin="round" strokeMiterlimit="10" d="
+		M62.498,22h-1.717L60,18.418c0-0.552-0.447-1-1-1s-1,0.448-1,1L57.219,22h-5.438L51,18.418c0-0.552-0.447-1-1-1s-1,0.448-1,1
+		L48.219,22h-5.438L42,18.418c0-0.552-0.447-1-1-1s-1,0.448-1,1L39.219,22h-5.438L33,18.418c0-0.552-0.448-1-1-1s-1,0.448-1,1
+		L30.218,22h-5.437L24,18.418c0-0.552-0.448-1-1-1s-1,0.448-1,1L21.218,22h-5.437L15,18.418c0-0.552-0.448-1-1-1s-1,0.448-1,1
+		L12.218,22H6.782L6,18.418c0-0.552-0.448-1-1-1s-1,0.448-1,1L3.218,22H1.5c-0.554,0-1,0.448-1,1v5.5v7v5.499
+		C0.5,41.552,0.946,42,1.5,42h1.718L4,45.582c0,0.552,0.447,1,1,1s1-0.448,1-1L6.782,42h5.437L13,45.582c0,0.552,0.447,1,1,1
+		s1-0.448,1-1L15.782,42h5.437L22,45.582c0,0.552,0.447,1,1,1s1-0.448,1-1L24.782,42h5.437L31,45.582c0,0.552,0.448,1,1,1
+		s1-0.447,1-1L33.781,42h5.438L40,45.582c0,0.553,0.447,1,1,1s1-0.447,1-1L42.781,42h5.438L49,45.582c0,0.553,0.447,1,1,1
+		s1-0.447,1-1L51.781,42h5.438L58,45.582c0,0.553,0.447,1,1,1s1-0.447,1-1L60.781,42h1.717c0.553,0,1.002-0.448,1.002-1.001V23
+		C63.5,22.448,63.051,22,62.498,22z"/>
+            </g>
+        )
+    }
+}
--- breadboardlab/src/utils/partsList.js
+++ breadboardlab/src/utils/partsList.js
@@ -10,35 +10,46 @@ import Battery from "../components/parts/Battery";
 import LEDv2 from "../components/parts/LEDv2";
 import NPNTransistor from "../components/parts/NPNTransistor";
 import PNPTransistor from "../components/parts/PNPTransistor";
+import QuadANDChip from "../components/parts/QuandANDChip";
+import QuadNANDChip from "../components/parts/QuandNANDChip";
+import QuadNORChip from "../components/parts/QuandNORChip";
+import QuadORChip from "../components/parts/QuandORChip";
+import QuadXORChip from "../components/parts/QuandXORChip";
 
 const partsList = {
     all: [
         {name: 'Breadboard', component: <BreadBoard/>, description: ''},
         {name: 'Battery', component: <Battery/>, description: ''},
-        {name: 'LED', component: <LED/>, description: 'A light-emitting diode that emits light when current flows through it.'},
-        {name: 'LEDv2', component: <LEDv2/>, description: 'A light-emitting diode that emits light when current flows through it.'},
+        //{name: 'LED', component: <LED/>, description: 'A light-emitting diode that emits light when current flows through it.'},
+        {name: 'LED', component: <LEDv2/>, description: 'A light-emitting diode that emits light when current flows through it.'},
         {name: 'Resistor', component: <Resistor/>, description: 'Limits and regulates the flow of electrical current.'},
         {name: 'Momentary Button', component: <MomentaryButton/>, description: 'A switch that closes or opens a circuit.'},
         {name: 'Switch', component: <Switch/>, description: ''},
         {name: 'Ceramic Capacitor', component: <CeramicCapacitor/>, description: ''},
-        {name: 'Transistor', component: <Transistor/>, description: ''},
+        //{name: 'Transistor', component: <Transistor/>, description: ''},
         {name: 'NPN Transistor', component: <NPNTransistor/>, description: ''},
         {name: 'PNP Transistor', component: <PNPTransistor/>, description: ''},
     ],
     basics: [
         {name: 'Breadboard', component: <BreadBoard/>, description: ''},
         {name: 'Battery', component: <Battery/>, description: ''},
-        {name: 'LED', component: <LED/>, description: 'A light-emitting diode that emits light when current flows through it.'},
-        {name: 'LEDv2', component: <LEDv2/>, description: 'A light-emitting diode that emits light when current flows through it.'},
+        //{name: 'LED', component: <LED/>, description: 'A light-emitting diode that emits light when current flows through it.'},
+        {name: 'LED', component: <LEDv2/>, description: 'A light-emitting diode that emits light when current flows through it.'},
         {name: 'Resistor', component: <Resistor/>, description: 'Limits and regulates the flow of electrical current.'},
         {name: 'Momentary Button', component: <MomentaryButton/>, description: 'A switch that closes or opens a circuit.'},
         {name: 'Switch', component: <Switch/>, description: ''},
         {name: 'Ceramic Capacitor', component: <CeramicCapacitor/>, description: ''},
-        {name: 'Transistor', component: <Transistor/>, description: ''},
+        //{name: 'Transistor', component: <Transistor/>, description: ''},
         {name: 'NPN Transistor', component: <NPNTransistor/>, description: ''},
         {name: 'PNP Transistor', component: <PNPTransistor/>, description: ''},
     ],
-    gates: []
+    gates: [
+        {name: 'Quad NAND Chip', component: <QuadNANDChip/>, description: '74HC00'},
+        {name: 'Quad NOR Chip', component: <QuadNORChip/>, description: '74HC02'},
+        {name: 'Quad AND Chip', component: <QuadANDChip/>, description: '74HC08'},
+        {name: 'Quad OR Chip', component: <QuadORChip/>, description: '74HC32'},
+        {name: 'Quad XOR Chip', component: <QuadXORChip/>, description: '74HC86'},
+    ]
 }
 
 
