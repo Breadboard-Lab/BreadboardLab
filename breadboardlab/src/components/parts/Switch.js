adc7fc6845d045b4c770e05066f763bb98d2e453
SethCohen <sethcohen.dev@gmail.com>
2021-07-23 13:26:40 -0400

    Changed/remade Switch graphics.

--- breadboardlab/src/components/parts/Switch.js
+++ breadboardlab/src/components/parts/Switch.js
@@ -12,7 +12,7 @@ export default class Switch extends React.Component {
             isSelected: false,
             isToggled: false
         }
-        this.scale = {x: 1, y: 1};
+        this.scale = {x: 0.65, y: 0.65};
         this.offSet = {x: 0, y: 0};
         this.onDoubleClick = this.onDoubleClick.bind(this);
         this.updateProp = this.updateProp.bind(this);
@@ -58,32 +58,25 @@ export default class Switch extends React.Component {
     render() {
         return (
             <g ref={this.node} onDoubleClick={this.onDoubleClick}>
-                <path id="pin1" fill="#707070" d="M17,46.272c0,0.553-0.448,1-1,1l0,0c-0.552,0-1-0.447-1-1L14,39c0-0.553,1.448-1,2-1l0,0
-		c0.552,0,2,0.447,2,1L17,46.272z"/>
-                <path id="pin2" fill="#707070" d="M33,46.272c0,0.553-0.448,1-1,1l0,0c-0.552,0-1-0.447-1-1L30,39c0-0.553,1.448-1,2-1l0,0
-		c0.552,0,2,0.447,2,1L33,46.272z"/>
-                <path id="pin3" fill="#707070" d="M49,46.272c0,0.553-0.447,1-1,1l0,0c-0.553,0-1-0.447-1-1L46,39c0-0.553,1.447-1,2-1l0,0
-		c0.553,0,2,0.447,2,1L49,46.272z"/>
-
-                <path id="body" fill="#3A3A3A" d="M63.5,37.667c0,2.75-2.25,5-5,5h-53c-2.75,0-5-2.25-5-5V26.333c0-2.75,2.25-5,5-5h53c2.75,0,5,2.25,5,5
-			V37.667z"/>
-                <rect id="innerbevel" x="16" y="24" fill="#141414" width="32" height="16"/>
-                <rect id="togglebevelbody" x={this.state.isToggled ? "32" : "18"} y="26" fill="#606161" width="14"
-                      height="12"/>
-                <rect id="togglebevel1" x={this.state.isToggled ? "32" : "18"} y="26" fill="#707071" width="2"
-                      height="12"/>
-                <rect id="togglebevel2" x={this.state.isToggled ? "36" : "22"} y="26" fill="#707071" width="2"
-                      height="12"/>
-                <rect id="togglebevel3" x={this.state.isToggled ? "40" : "26"} y="26" fill="#707071" width="2"
-                      height="12"/>
-                <rect id="togglebevel4" x={this.state.isToggled ? "44" : "30"} y="26" fill="#707071" width="2"
-                      height="12"/>
-
-                <path id="select" fill="none" stroke={this.state.isSelected ? "#2453ff" : "none"} strokeMiterlimit="10"
-                      d="M58.5,21.333h-53c-2.75,0-5,2.25-5,5v11.334
-		c0,2.75,2.25,5,5,5h9.004L15,46.272c0,0.553,0.448,1,1,1s1-0.447,1-1l0.496-3.605h13.009L31,46.272c0,0.553,0.448,1,1,1
-		s1-0.447,1-1l0.496-3.605h13.008L47,46.272c0,0.553,0.447,1,1,1s1-0.447,1-1l0.496-3.605H58.5c2.75,0,5-2.25,5-5V26.333
-		C63.5,23.583,61.25,21.333,58.5,21.333z"/>
+                <path id="pin1" fill="#606161" d="M10.023,47.095c0,0.81-0.657,1.465-1.465,1.465l0,0c-0.809,0-1.465-0.655-1.465-1.465
+		L5.628,36.441c0-0.811,2.122-1.466,2.93-1.466l0,0c0.809,0,2.93,0.655,2.93,1.466L10.023,47.095z"/>
+                <path id="pin2" fill="#606161" d="M33.465,47.095c0,0.81-0.656,1.465-1.465,1.465l0,0c-0.809,0-1.465-0.655-1.465-1.465
+		L29.07,36.441c0-0.811,2.122-1.466,2.93-1.466l0,0c0.809,0,2.931,0.655,2.931,1.466L33.465,47.095z"/>
+                <path id="pin3" fill="#606161" d="M56.907,47.095c0,0.81-0.655,1.465-1.466,1.465l0,0c-0.81,0-1.465-0.655-1.465-1.465
+		l-1.465-10.653c0-0.811,2.12-1.466,2.93-1.466l0,0c0.811,0,2.931,0.655,2.931,1.466L56.907,47.095z"/>
+                <path id="body" fill="#3A3A3A" d="M63.5,35.631c0,3.399-2.251,6.182-5.001,6.182H5.5c-2.75,0-5-2.782-5-6.182V21.622
+		c0-3.399,2.25-6.181,5-6.181h52.999c2.75,0,5.001,2.781,5.001,6.181V35.631z"/>
+                <rect id="innerbevel" x="8.558" y="19.197" fill="#141414" width="46.883" height="18.861"/>
+                <rect id="togglebevelbody" x={this.state.isToggled ? "32" : "11.49"} y="21.666" fill="#606161" width="20.376" height="13.919"/>
+                <rect id="togglebevel1" x={this.state.isToggled ? "32" : "11.49"} y="21.666" fill="#707071" width="2.93" height="13.922"/>
+                <rect id="togglebevel2" x={this.state.isToggled ? "37.859" : "17.349"} y="21.665" fill="#707071" width="2.931" height="13.92"/>
+                <rect id="togglebevel3" x={this.state.isToggled ? "43.72" : "23.209"} y="21.666" fill="#707071" width="2.93" height="13.922"/>
+                <rect id="togglebevel4" x={this.state.isToggled ? "49.58" : "29.07"} y="21.665" fill="#707071" width="2.93" height="13.92"/>
+                <path id="select" fill="none" stroke={this.state.isSelected ? "#2453ff" : "none"}  strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="10" d="
+		M58.499,15.441H5.5c-2.75,0-5,2.781-5,6.181v14.009c0,3.399,2.25,6.182,5,6.182h0.867l0.727,5.282c0,0.81,0.656,1.465,1.465,1.465
+		s1.465-0.655,1.465-1.465l0.727-5.282h19.059l0.726,5.282c0,0.81,0.657,1.465,1.465,1.465s1.465-0.655,1.465-1.465l0.727-5.282
+		H53.25l0.727,5.282c0,0.81,0.655,1.465,1.465,1.465c0.811,0,1.466-0.655,1.466-1.465l0.727-5.282h0.865
+		c2.75,0,5.001-2.782,5.001-6.182V21.622C63.5,18.222,61.249,15.441,58.499,15.441z"/>
             </g>
         )
     }
