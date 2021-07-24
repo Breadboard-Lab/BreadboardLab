89ff09a48fb0114edad5610f734b17b5140feab8
Benedict Leung <benedict.leung1@ontariotechu.net>
2021-07-24 18:45:34 -0400

    Added connection to LEDv2

--- breadboardlab/src/components/parts/LEDv2.js
+++ breadboardlab/src/components/parts/LEDv2.js
@@ -1,10 +1,14 @@
 import React from "react";
 import interact from "interactjs";
+import Interactable from "../Interactable";
 
 export default class LEDv2 extends React.Component {
     constructor(props) {
         super(props);
         this.node = React.createRef();
+        this.connectorContainer = React.createRef();
+        this.cathode = React.createRef();
+        this.anode = React.createRef();
 
         this.state = {
             type: "LED",
@@ -13,21 +17,65 @@ export default class LEDv2 extends React.Component {
             bodyColourRim: "hsl(0, 75%, 50%)",
             bodyColourTop: "hsl(0, 75%, 64%)",
             bodyColourBottom: "hsl(0, 75%, 59%)",
+            cathodePoint: {x: 24, y: 62.5},
+            anodePoint: {x: 40, y: 62.5},
             isSelected: false,
             translation: {x: 0, y: 0},
             rotation: 0
         }
-        this.scale = {x: 1, y: 1};
-        this.offSet = {x: -15.3605, y: -0.5};
         this.onDoubleClick = this.onDoubleClick.bind(this);
         this.updateProp = this.updateProp.bind(this);
+
+        this.scale = {x: 1, y: 1};
+        this.offSet = {x: -15.3605, y: -0.5};
+        this.attachTo = new Map();
+        this.refArray = [
+            {id: "cathode", ref: this.cathode},
+            {id: "anode", ref: this.anode},
+        ];
+    }
+
+    draggableOptionsCathode = {
+        listeners: {
+            move: (event) => {
+                let scale = document.getElementById("AppSVG").getAttribute("scale");
+
+                this.setState({
+                    cathodePoint: {
+                        x: Number(event.currentTarget.getAttribute("cx")) + event.delta.x * scale / this.scale.x,
+                        y: Number(event.currentTarget.getAttribute("cy")) + event.delta.y * scale / this.scale.y
+                    }
+                });
+            }
+        }
+    }
+
+    draggableOptionsAnode = {
+        listeners: {
+            move: (event) => {
+                let scale = document.getElementById("AppSVG").getAttribute("scale");
+
+                this.setState({
+                    anodePoint: {
+                        x: Number(event.currentTarget.getAttribute("cx")) + event.delta.x * scale / this.scale.x,
+                        y: Number(event.currentTarget.getAttribute("cy")) + event.delta.y * scale / this.scale.y
+                    }
+                });
+            }
+        }
     }
 
     componentDidMount() {
         interact(this.node.current).styleCursor(false).draggable({
             listeners: {
-                move: (event) => {
-                    this.props.movePart(event, this)
+                move: event => {
+                    if (event.currentTarget === this.connectorContainer.current && typeof this.props.movePart === "function") {
+                        this.props.movePart(event, this);
+                    } else {
+                        const {interaction} = event;
+                        interaction.stop()
+                        interaction.start({name: "drag"}, event.interactable, this.connectorContainer.current);
+                    }
                 }
             },
         })
@@ -105,6 +153,116 @@ export default class LEDv2 extends React.Component {
         )
     }
 
+    highlight(event, attachRef) {
+        let elementID = this.checkConnected(attachRef);
+        this.highlightID = {ids: elementID, ref: attachRef};
+
+        for (let connectorID of this.highlightID.ids)
+            if (connectorID)
+                attachRef.node.current.querySelector("#" + connectorID).setAttribute("filter", "url(#f3)")
+    }
+
+    connect(relatedTarget, currentTarget, attachRef) {
+        if (this.highlightID) {
+            for (let i = 0; i < this.refArray.length; i++) {
+                if (this.highlightID.ids[i]) {
+                    const xPos = (Number(attachRef.node.current.querySelector("#" + this.highlightID.ids[i]).getAttribute("cx")) + attachRef.offSet.x) * attachRef.scale.x / this.scale.x - this.offSet.x + (attachRef.state.translation.x - this.state.translation.x) / this.scale.x;
+                    const yPos = (Number(attachRef.node.current.querySelector("#" + this.highlightID.ids[i]).getAttribute("cy")) + attachRef.offSet.y) * attachRef.scale.y / this.scale.x - this.offSet.y + (attachRef.state.translation.y - this.state.translation.y) / this.scale.y;
+
+                    this.attachTo.set(this.refArray[i].id, {id: this.highlightID.ids[i], ref: attachRef});
+
+                    if (typeof attachRef.connectPart === "function") {
+                        attachRef.connectPart(this.highlightID.ids[i], this.refArray[i].id, this);
+                    }
+                    this.moveConnector(this.refArray[i].ref.current.node, xPos, yPos);
+                }
+            }
+        }
+        
+    }
+
+    disconnect(event) {
+        if (event && (event.relatedTarget === this.anode.current.node || event.relatedTarget === this.cathode.current.node)) {
+            if (event.relatedTarget === this.anode.current.node) {
+                if (this.attachTo.get("anode") && typeof this.attachTo.get("anode").ref.disconnectPart === "function") {
+                    this.attachTo.get("anode").ref.disconnectPart(this.attachTo.get("anode").id, this);
+                }
+                this.attachTo.set("anode", undefined);
+                event.currentTarget.setAttribute("filter", "");
+            } else if (event.relatedTarget === this.cathode.current.node) {
+                if (this.attachTo.get("cathode") && typeof this.attachTo.get("cathode").ref.disconnectPart === "function") {
+                    this.attachTo.get("cathode").ref.disconnectPart(this.attachTo.get("cathode").id, this);
+                }
+                this.attachTo.set("cathode", undefined);
+                event.currentTarget.setAttribute("filter", "");
+            }
+        } else {
+            if (this.highlightID)
+                for (let id of this.highlightID.ids)
+                    if (id)
+                        this.highlightID.ref.node.current.querySelector("#" + id).setAttribute("filter", "");
+
+            for (let refData of this.refArray) {
+                if (this.attachTo.get(refData.id) && typeof this.attachTo.get(refData.id).ref.disconnectPart === "function") {
+                    this.attachTo.get(refData.id).ref.disconnectPart(this.attachTo.get(refData.id).id, this);
+                }
+                this.attachTo.set(refData.id, undefined);
+            }
+        }
+    }
+
+    moveConnector(connector, xPos, yPos) {
+        if (connector === this.cathode.current.node) {
+            this.setState({cathodePoint: {x: xPos, y: yPos}});
+        } else {
+            this.setState({anodePoint: {x: xPos, y: yPos}});
+        }
+    }
+
+    checkConnected(attachRef) {
+        let elementID = [];
+        let connectors = Array.prototype.slice.call(attachRef.connectors);
+
+        if (connectors) {
+            for (let refData of this.refArray) {
+                let element = undefined;
+
+                for (let connector of connectors) {
+                    let rect1 = refData.ref.current.node.getBoundingClientRect();
+                    let rect2 = connector.getBoundingClientRect();
+                    let overlap = !(rect1.right < rect2.left || rect1.left > rect2.right || rect1.bottom < rect2.top || rect1.top > rect2.bottom);
+
+                    if (overlap) {
+                        element = connector;
+                        break;
+                    }
+                }
+
+                if (element) {
+                    let rect1 = refData.ref.current.node.getBoundingClientRect();
+                    let rect2 = element.getBoundingClientRect();
+                    let overlap = !(rect1.right < rect2.left || rect1.left > rect2.right || rect1.bottom < rect2.top || rect1.top > rect2.bottom);
+
+                    if (overlap && attachRef.connectedParts && (attachRef.connectedParts.get(element.id) === undefined || attachRef.connectedParts.get(element.id).ref === this))
+                        elementID.push(element.id);
+                    else
+                        elementID.push(undefined);
+                } else {
+                    elementID.push(undefined);
+                }
+            }
+        }
+        return elementID;
+    }
+
+    onMouseEnter(event) {
+        event.target.setAttribute("style", "cursor: move");
+    }
+
+    onMouseLeave(event) {
+        event.target.setAttribute("style", "");
+    }
+
     render() {
         let rotatePointX;
         let rotatePointY;
@@ -133,6 +291,34 @@ export default class LEDv2 extends React.Component {
                 C45.288,6.863,38.731,0.5,30.644,0.5z"/>
                     <path id="bottom" fill={this.state.bodyColourBottom} d="M45.282,33.22c0,5.662-6.554,10.251-14.639,10.251
                 c-8.084,0-14.637-4.589-14.637-10.251c0-5.659,6.553-10.248,14.637-10.248C38.729,22.972,45.282,27.561,45.282,33.22z"/>
+                
+                    <path stroke="#707071" strokeWidth="4" strokeLinecap="round"
+                            d={`M ${24} ${62.5} L ${this.state.cathodePoint.x} ${this.state.cathodePoint.y}`}/>
+                    <path stroke="#707071" strokeWidth="4" strokeLinecap="round"
+                            d={`M ${40} ${62.5} L ${this.state.anodePoint.x} ${this.state.anodePoint.y}`}/>
+
+                    <g ref={this.connectorContainer} className="connector">
+                        <Interactable 
+                            ref={this.cathode} styleCursor={false} draggable={true} draggableOptions={this.draggableOptionsCathode}>
+                            <ellipse
+                                onMouseEnter={this.onMouseEnter}
+                                onMouseLeave={this.onMouseLeave}
+                                className="connector" strokeOpacity="1" fillOpacity="0" stroke="black"
+                                cx={this.state.cathodePoint.x} cy={this.state.cathodePoint.y}
+                                rx="2" ry="2"/>
+                        </Interactable>
+
+                        <Interactable 
+                            ref={this.anode} styleCursor={false} draggable={true} draggableOptions={this.draggableOptionsAnode}>
+                            <ellipse
+                                onMouseEnter={this.onMouseEnter}
+                                onMouseLeave={this.onMouseLeave}
+                                className="connector" strokeOpacity="1" fillOpacity="0" stroke="black"
+                                cx={this.state.anodePoint.x} cy={this.state.anodePoint.y}
+                                rx="2" ry="2"/>
+                        </Interactable>
+                    </g>
+
                     <path id="select" fill="none" stroke={this.state.isSelected ? "#2453ff" : "none"} strokeLinecap="round"
                         strokeLinejoin="round" strokeMiterlimit="10" d="
                 M48,35.091c0-3.75-1.003-6.422-2.712-8.327V14.712c0-7.849-6.557-14.212-14.645-14.212C22.557,0.5,16,6.863,16,14.712v14.666
