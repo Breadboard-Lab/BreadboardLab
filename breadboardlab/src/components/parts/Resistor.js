f578064aefac79825dd1641389c8f12825ac3ac9
Benedict Leung <benedict.leung1@ontariotechu.net>
2021-08-04 00:49:00 -0400

    Resistor now rotates automatically

--- breadboardlab/src/components/parts/Resistor.js
+++ breadboardlab/src/components/parts/Resistor.js
@@ -356,35 +356,35 @@ export default class Resistor extends React.Component {
             let partBBox = this.node.current.firstChild.getBBox();
             rotatePointX = partBBox.width / 2;
             rotatePointY = partBBox.height / 2;
-            
         } else {
             rotatePointX = 0;
             rotatePointY = 0;
         }
+
+        let angle = Math.atan2((this.state.rightPoint.y - this.state.leftPoint.y), this.state.rightPoint.x - this.state.leftPoint.x );
         
         return (
             <g ref={this.node} onDoubleClick={this.onDoubleClick} transform={`translate(${this.state.translation.x} ${this.state.translation.y})`}>
                 <g transform={this.props.icon ? `translate(100,105) scale(200,150)` : `scale(${this.scale.x} ${this.scale.y}) rotate(${this.state.rotation} ${rotatePointX} ${rotatePointY}) translate(${this.offSet.x} ${this.offSet.y})`}>
                     <path stroke="#707071" strokeWidth="0.036" strokeLinecap="round"
-                            d={`M ${(this.state.leftPoint.x + this.state.rightPoint.x + 0.5 + 0.182) / 2 - 0.5} ${(this.state.leftPoint.y + this.state.rightPoint.y + 0.453 + 0.453) / 2 - 0.453} L ${this.state.leftPoint.x} ${this.state.leftPoint.y}`}/>
-                    <path stroke="#707071" strokeWidth="0.036" strokeLinecap="round"
-                            d={`M ${(this.state.leftPoint.x + this.state.rightPoint.x + 0.5 + 0.182) / 2 - 0.182} ${(this.state.leftPoint.y + this.state.rightPoint.y + 0.453 + 0.453) / 2 - 0.453} L ${this.state.rightPoint.x} ${this.state.rightPoint.y}`}/>
+                            d={`M ${this.state.leftPoint.x} ${this.state.leftPoint.y} L ${this.state.rightPoint.x} ${this.state.rightPoint.y}`}/>
 
                     <g transform={`translate(${(this.state.leftPoint.x + this.state.rightPoint.x + 0.5 + 0.182) / 2} ${(this.state.leftPoint.y + this.state.rightPoint.y + 0.453 + 0.453) / 2})`}>
-                        <path fill="#A08968" d="M -0.5 -0.435 c 0.025 0 0.036 0.053 0.053 0.053 h 0.028 c 0.009 -0.005 0.017 -0.007 0.021 -0.007 h 0.071 h 0.043
-                        c 0.009 0.005 0.017 0.007 0.021 0.007 h 0.028 c 0.018 0 0.028 -0.053 0.053 -0.053 v -0.036 c -0.025 0 -0.036 -0.053 -0.053 -0.053 h -0.027 c -0.005 0 -0.012 0.002 -0.021 0.007 h -0.122
-                        c 0 0 -0.005 -0.002 -0.014 -0.007 h -0.029 c -0.018 0 -0.028 0.053 -0.052 0.053z"/>
-                        <path fill={this.state.band1Colour}
-                            d="M-0.447-0.382c0.009,0.005,0.019,0.005,0.028,0v-0.143c-0.009-0.005-0.019-0.005-0.028,0"/>
-                        <path fill={this.state.band2Colour} d="M-0.378-0.389h0.025v-0.129h-0.025"/>
-                        <path fill={this.state.band3Colour} d="M-0.325-0.389H-0.3v-0.129h-0.025"/>
-                        <path fill={this.state.band4Colour}
-                            d="M-0.261-0.382c0.009,0.005,0.019,0.005,0.028,0v-0.143c-0.009-0.005-0.019-0.005-0.028,0"/>
-                        <path fill="none" stroke={this.state.isSelected ? "#2453ff" : "none"} strokeWidth="0.015"
-                            strokeMiterlimit="50" strokeLinecap="round" strokeLinejoin="round" d="M -0.5 -0.435 c 0.025 0 0.036 0.053 0.053 0.053 h 0.028 c 0.009 -0.005 0.017 -0.007 0.021 -0.007 h 0.071 h 0.043
-                        c 0.009 0.005 0.017 0.007 0.021 0.007 h 0.028 c 0.018 0 0.028 -0.053 0.053 -0.053 v -0.036 c -0.025 0 -0.036 -0.053 -0.053 -0.053 h -0.027 c -0.005 0 -0.012 0.002 -0.021 0.007 h -0.122
-                        c 0 0 -0.005 -0.002 -0.014 -0.007 h -0.029 c -0.018 0 -0.028 0.053 -0.052 0.053z"/>
-                        
+                        <g transform={`rotate(${angle * 180 / Math.PI} ${-0.344} ${-0.453})`}>
+                            <path fill="#A08968" d="M -0.5 -0.435 c 0.025 0 0.036 0.053 0.053 0.053 h 0.028 c 0.009 -0.005 0.017 -0.007 0.021 -0.007 h 0.071 h 0.043
+                            c 0.009 0.005 0.017 0.007 0.021 0.007 h 0.028 c 0.018 0 0.028 -0.053 0.053 -0.053 v -0.036 c -0.025 0 -0.036 -0.053 -0.053 -0.053 h -0.027 c -0.005 0 -0.012 0.002 -0.021 0.007 h -0.122
+                            c 0 0 -0.005 -0.002 -0.014 -0.007 h -0.029 c -0.018 0 -0.028 0.053 -0.052 0.053z"/>
+                            <path fill={this.state.band1Colour}
+                                d="M-0.447-0.382c0.009,0.005,0.019,0.005,0.028,0v-0.143c-0.009-0.005-0.019-0.005-0.028,0"/>
+                            <path fill={this.state.band2Colour} d="M-0.378-0.389h0.025v-0.129h-0.025"/>
+                            <path fill={this.state.band3Colour} d="M-0.325-0.389H-0.3v-0.129h-0.025"/>
+                            <path fill={this.state.band4Colour}
+                                d="M-0.261-0.382c0.009,0.005,0.019,0.005,0.028,0v-0.143c-0.009-0.005-0.019-0.005-0.028,0"/>
+                            <path fill="none" stroke={this.state.isSelected ? "#2453ff" : "none"} strokeWidth="0.015"
+                                strokeMiterlimit="50" strokeLinecap="round" strokeLinejoin="round" d="M -0.5 -0.435 c 0.025 0 0.036 0.053 0.053 0.053 h 0.028 c 0.009 -0.005 0.017 -0.007 0.021 -0.007 h 0.071 h 0.043
+                            c 0.009 0.005 0.017 0.007 0.021 0.007 h 0.028 c 0.018 0 0.028 -0.053 0.053 -0.053 v -0.036 c -0.025 0 -0.036 -0.053 -0.053 -0.053 h -0.027 c -0.005 0 -0.012 0.002 -0.021 0.007 h -0.122
+                            c 0 0 -0.005 -0.002 -0.014 -0.007 h -0.029 c -0.018 0 -0.028 0.053 -0.052 0.053z"/>
+                        </g>
                         <g ref={this.connectorContainer} className="connector">
                             <Interactable 
                                 ref={this.left} styleCursor={false} draggable={true} draggableOptions={this.draggableOptionsLeft}>
